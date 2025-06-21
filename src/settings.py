f7acf6cc5552b6c1b2981b93a9a58ed7fa8ac920
ademic2022 <adeeyomichael77@gmail.com>
2025-06-21 14:31:13 +0100

    Update ALLOWED_HOSTS to allow all origins for development

--- src/settings.py
+++ src/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = "django-insecure-5ffe$jikr-x1%40n14^c9z%2v+16=m-28ei*ci#2e%a&d+krt(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 # Application definition
