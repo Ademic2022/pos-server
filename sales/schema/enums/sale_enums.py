989cad968c3a9a239fb5d653a0d9bf760ab9ae0d
ademic2022 <adeeyomichael77@gmail.com>
2025-07-01 16:42:44 +0100

    feat(sales): Refactor GraphQL enums to use choices for improved maintainability

--- sales/schema/enums/sale_enums.py
+++ sales/schema/enums/sale_enums.py
@@ -9,24 +9,19 @@ from sales.choices import SaleTypeChoices, PaymentMethodChoices, TransactionType
 class SaleTypeEnum(graphene.Enum):
     """GraphQL enum for sale types"""
 
-    RETAIL = SaleTypeChoices.RETAIL.value
-    WHOLESALE = SaleTypeChoices.WHOLESALE.value
+    class Meta:
+        enum = SaleTypeChoices
 
 
 class PaymentMethodEnum(graphene.Enum):
     """GraphQL enum for payment methods"""
 
-    CASH = PaymentMethodChoices.CASH.value
-    TRANSFER = PaymentMethodChoices.TRANSFER.value
-    CREDIT = PaymentMethodChoices.CREDIT.value
-    PART_PAYMENT = PaymentMethodChoices.PART_PAYMENT.value
+    class Meta:
+        enum = PaymentMethodChoices
 
 
 class TransactionTypeEnum(graphene.Enum):
     """GraphQL enum for transaction types"""
 
-    CREDIT_ADDED = TransactionTypeChoices.CREDIT_ADDED.value
-    CREDIT_USED = TransactionTypeChoices.CREDIT_USED.value
-    CREDIT_REFUND = TransactionTypeChoices.CREDIT_REFUND.value
-    CREDIT_EARNED = TransactionTypeChoices.CREDIT_EARNED.value
-    DEBT_INCURRED = TransactionTypeChoices.DEBT_INCURRED.value
+    class Meta:
+        enum = TransactionTypeChoices
