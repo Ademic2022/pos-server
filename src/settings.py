59d018edf81ab0bd17d99fe789634c6a41b2fc10
Michael <adeeyomichael77@gmail.com>
2025-08-26 09:11:51 +0100

    update settings

--- src/settings.py
+++ src/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-5ffe$jikr-x1%40n14^c9z%2v+16=m-28ei*ci#2e%a&d+krt("
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 CORS_ALLOW_ALL_ORIGINS = True
